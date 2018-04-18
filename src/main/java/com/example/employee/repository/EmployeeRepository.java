99050fb6784b032578298d598223ceb65999e4ac
LisaHuang <1449700087@qq.com>
2018-04-18 20:33:17 +0800

    kepp table name consistent

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -19,11 +19,11 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //2.找出Employee表中第一个姓名包含`*`字符并且薪资大于*的雇员个人信息
     Employee getFirstByNameContainingAndSalaryGreaterThan(String nameContains,Integer salary);
 
-    @Query(value = "select * from employee where name like %:nameContains% and salary > :salary limit 1", nativeQuery = true)
+    @Query(value = "select * from Employee where name like %:nameContains% and salary > :salary limit 1", nativeQuery = true)
     Employee getFirstEmployeeByNameCharacterAndSalary(@Param("nameContains")String nameContains,@Param("salary")Integer salary);
 
     //3.找出一个薪资最高且公司ID是*的雇员以及该雇员的姓名
-    @Query(value = "SELECT * FROM employee WHERE companyId =?1 ORDER BY salary DESC limit 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM Employee WHERE companyId =?1 ORDER BY salary DESC limit 1", nativeQuery = true)
     Employee getTopSalaryEmployeeByCompanyId(Integer companyId);
 
     //4.实现对Employee的分页查询，每页两个数据
