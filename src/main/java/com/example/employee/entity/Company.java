88dfbb12c9626f07898639b0fb88803f75b3e470
LisaHuang <1449700087@qq.com>
2018-04-18 19:54:06 +0800

    complete entity Company.java

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,44 @@
 package com.example.employee.entity;
 
-public class Company {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name ="Company")
+public class Company {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "companyName", nullable = false)
+    private String companyName;
+
+    @Column(name = "employeesNumber", nullable = false)
+    private String employeesNumber;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getEmployeesNumber() {
+        return employeesNumber;
+    }
+
+    public void setEmployeesNumber(String employeesNumber) {
+        this.employeesNumber = employeesNumber;
+    }
 }
